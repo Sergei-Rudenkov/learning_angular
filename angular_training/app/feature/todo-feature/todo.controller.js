49913ed0d055470eba2843b1b674a332e54a2af8
Sergei-Rudenkov <Sergei_Rudenkov@epam.com>
2016-11-14 00:56:11 +0300

    filter experement

--- angular_training/app/feature/todo-feature/todo.controller.js
+++ angular_training/app/feature/todo-feature/todo.controller.js
@@ -7,7 +7,7 @@
     angular.module("todo-feature")
         .controller('Todo', Todo);
 
-    function Todo(model, todoService, lsService) {
+    function Todo(model, todoService, lsService, $filter) {
         let $ctrl = this;
         $ctrl.todo = model;
         $ctrl.newItem = {};
@@ -17,6 +17,21 @@
         // Object.assign($ctrl, starServiceProvider);
         // console.log(starServiceProvider.evaluate(980));
         $ctrl.showComplete = false;
+            $ctrl.startFrom = 0;
+        $ctrl.limit = 2;
         $ctrl.sortItemsReverse = false;
+        $ctrl.decrementPage = function () {
+            $ctrl.startFrom = $ctrl.startFrom - $ctrl.limit;
+            console.log($ctrl.startFrom);
+        };
+        $ctrl.incrementPage = function () {
+            $ctrl.startFrom = $ctrl.startFrom + $ctrl.limit;
+            console.log($ctrl.startFrom);
+        };
+        $ctrl.todo.paginatedItems = $filter('paginationFilter')([$ctrl.todo, $ctrl.limit, $ctrl.startFrom]);
+        $ctrl.todo.paginatedItems.forEach(function (item) {
+            console.log(item)
+        })
+
     }
 })();
