b96871466842ff113d05f361becc704ace29d486
Sergei-Rudenkov <Sergei_Rudenkov@epam.com>
2016-11-23 17:51:01 +0300

    implement promises

--- angular_training/app/feature/todo-table/todo.module.js
+++ angular_training/app/feature/todo-table/todo.module.js
@@ -4,15 +4,8 @@
 (function () {
     "use strict";
     angular.module("todo-feature", ["paging-module"])
-        .run(runApp)
         .value("model", {
             "user": "Sergei",
             "userPhoto": "./images/SR.png"
         });
-
-    function runApp($http, model) {
-        $http
-            .get("./feature/todo-table/todo.json")
-            .then((response) => model.items = response.data);
-    }
 })();
